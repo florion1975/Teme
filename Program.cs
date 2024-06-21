ff30fb13ea9de863792a118ba37fba510bb8cc56
Ion Floarea <166043585+florion1975@users.noreply.github.com>
2024-06-21 14:19:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MediaAritmetica
+{
+    class MediaAritmetica
+{
+    static void Main(string[] args) {
+
+            //double lungime = 24.5, latime = 11.23, inaltime = 15.7;
+
+            //double MediaAritmetica = (lungime * latime * inaltime)/3;
+
+
+            //Console.Write("Rezultatul este: ");
+            //Console.WriteLine(MediaAritmetica);
+
+
+
+
+            Console.WriteLine("Introduceti lungimea");
+            double lungime = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti latimea");
+            double latime = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti inaltimea");
+            double inaltime = double.Parse(Console.ReadLine());
+
+            double MediaAritmetica = (lungime * latime * inaltime) / 3;
+
+            Console.Write("Rezultatul este: ");
+            Console.WriteLine(MediaAritmetica);
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
