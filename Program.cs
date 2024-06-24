399f09cd5befbbbf9ffd7dd07da3eb9221e76327
Ion Floarea <166043585+florion1975@users.noreply.github.com>
2024-06-24 10:12:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 
-namespace MediaAritmetica
+namespace TemeDeCasa
 {
-    class MediaAritmetica
+    class Rezolvari
 {
     static void Main(string[] args) {
+
+            //double lungime = 24.5, latime = 11.23, inaltime = 15.7;
+
+
+            //double mediaAritmetica = (lungime * latime * inaltime);
+
+
+            //Console.Write("Rezultatul este: ");
+            //Console.WriteLine(calculVolum);
+
+
+
+
+            // Console.WriteLine("Introduceti lungimea");
+            // double lungime = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Introduceti latimea");
+            // double latime = double.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Introduceti inaltimea");
+            // double inaltime = double.Parse(Console.ReadLine());
+
+
+            // double calculVolum = (lungime * latime * inaltime);
+
+            // Console.Write("Rezultatul este: ");
+            // Console.WriteLine(calculVolum);
+
+
+        
 
             //double lungime = 24.5, latime = 11.23, inaltime = 15.7;
 
