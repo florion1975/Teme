8eae29fbd65b267eeead2f9130c2c0f52db57306
Ion Floarea <166043585+florion1975@users.noreply.github.com>
2024-07-01 17:31:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -62,6 +62,98 @@ namespace TemeDeCasa
             Console.WriteLine(MediaAritmetica);
 
 
+        using System;
+
+namespace CelMaiMicNumar 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Introduceti primul numar:");
+            //double numar1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Introduceti al doilea numar:");
+            //double numar2 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Introduceti al treilea numar:");
+            //double numar3 = double.Parse(Console.ReadLine());
+
+            //double minim = 0;
+
+            //  if (numar1 < minim)
+            //  {
+            //      minim = numar1;
+            //  }
+            //  if (numar2 < minim)
+            //  {
+            //      minim = numar2;
+            //  }
+            //  if (numar3 < minim)
+            //  {
+            //      minim = numar3;
+            //  }
+            //  Console.WriteLine("Cel mai mic numar este: " + minim);
+
+            //// Determinare cel mai mic numar
+            //double minim = numar1;
+
+            //if (numar2 < minim)
+            //{
+            //    minim = numar2;
+            //    Console.WriteLine("Cel mai mic numar este: " + minim);
+            //}
+
+            //else if (numar3 < minim)
+            //{
+            //    minim = numar3;
+            //    Console.WriteLine("Cel mai mic numar este: " + minim);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Numerele sunt egale");
+            //}
+            //Console.WriteLine("operatia a fost finalizata cu succes");
+
+            Console.WriteLine("Introduceti primul numar:");
+            double numar1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al doilea numar:");
+            double numar2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti al treilea numar:");
+            double numar3 = double.Parse(Console.ReadLine());
+
+            double minim = 0;
+
+
+            if (numar1 < minim)
+            {
+                minim = numar1;               
+            }
+            else if (numar2 < minim)
+            {
+                minim = numar2;               
+            }
+            else if (numar3 < minim)
+            {
+                minim = numar3;                
+            } else
+            {
+                Console.WriteLine("Numerele sunt egale");
+            }
+
+            Console.WriteLine($"Cel mai mic numar este: {minim}");
+
+
+
+        }
+    }
+}
+
+           
+
+
 
 
 
